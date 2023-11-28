7ca3f151782449ca78a0aef0b14f1dd02ce753db
Randy Arakawa <randy.arakawa@gmail.com>
2023-11-28 09:37:43 -1000

    add student id
    add student id function

--- studentdatabaseapp/src/Student.java
+++ studentdatabaseapp/src/Student.java
@@ -4,17 +4,39 @@ public class Student {
     private String firstName;
     private String lastName;
     private String gradeYear;
-    private int studentID;
+    private String studentID;
     private String courses;
     private int tuitionBalance;
-    private int costOFCourse = 600;
+    private static int costOFCourse = 600;
+    private static int id = 1000;
 
     // Constructor: prompt user to enter student's name and year
+    
     public Student() {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter student first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.println("Enter student last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.println("1 - Freshmen\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter student class level: ");
+        this.gradeYear = in.nextLine();
+
+        setStudentID();
+
+        System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+
+
     }
+    
     // Generate an ID
+    private void setStudentID() {
+        // Grade Level + ID
+        id++;
+        this.studentID =  gradeYear + "" + id;
+    }
+
 
     // Enroll in courses
 
--- studentdatabaseapp/src/StudentDatabaseApp.java
+++ studentdatabaseapp/src/StudentDatabaseApp.java
@@ -1,5 +1,6 @@
 public class StudentDatabaseApp {
     public static void main(String[] args) {
+        Student person1 = new Student();
         // Ask how many new users we want to add
 
         // Create n number of students
