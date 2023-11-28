b19f7527bf113919f8a1690e79a95c504b1a2e3e
Randy Arakawa <randy.arakawa@gmail.com>
2023-11-28 09:10:34 -1000

    initial commit
    initial commit

---
+++ studentdatabaseapp/src/Student.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private int costOFCourse = 600;
+
+    // Constructor: prompt user to enter student's name and year
+    public Student() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter student first name: ");
+    }
+    // Generate an ID
+
+    // Enroll in courses
+
+    // View balance
+
+    // Pay tuition
+
+    // Show status
+}
